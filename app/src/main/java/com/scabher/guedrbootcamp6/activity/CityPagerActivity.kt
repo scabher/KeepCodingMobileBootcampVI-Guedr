43da36e2dada63372950087a4c77ec75581af34e
Sergio <sergio.cabrera.hernandez@gmail.com>
2018-05-15 21:06:26 +0100

    Add menu items (previous and next buttons)

--- app/src/main/java/com/scabher/guedrbootcamp6/activity/CityPagerActivity.kt
+++ app/src/main/java/com/scabher/guedrbootcamp6/activity/CityPagerActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
+import android.view.Menu
+import android.view.MenuItem
 import com.scabher.guedrbootcamp6.R
 import com.scabher.guedrbootcamp6.fragment.ForecastFragment
 import com.scabher.guedrbootcamp6.model.Cities
@@ -35,5 +37,37 @@ class CityPagerActivity : AppCompatActivity() {
         }
 
         view_pager.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.pager_navigation, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?)= when (item?.itemId) {
+        R.id.previous -> {
+            view_pager.currentItem -= 1
+            true
+        }
+        R.id.next -> {
+            view_pager.currentItem += 1
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        super.onPrepareOptionsMenu(menu)
+
+        val previousMenu = menu?.findItem(R.id.previous)
+        val nextMenu = menu?.findItem(R.id.next)
+        val adapter = view_pager.adapter!!
+
+        previousMenu?.isEnabled = view_pager.currentItem > 0
+        nextMenu?.isEnabled = view_pager.currentItem < adapter.count - 1
+
+        return true
     }
 }
