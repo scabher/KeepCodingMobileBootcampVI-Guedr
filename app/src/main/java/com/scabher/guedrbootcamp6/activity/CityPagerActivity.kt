dbabf221e9296bf826da2c1411a672c83028ba90
Sergio <sergio.cabrera.hernandez@gmail.com>
2018-05-15 21:48:37 +0100

    Show City name in toolbar

--- app/src/main/java/com/scabher/guedrbootcamp6/activity/CityPagerActivity.kt
+++ app/src/main/java/com/scabher/guedrbootcamp6/activity/CityPagerActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import android.view.Menu
 import android.view.MenuItem
 import com.scabher.guedrbootcamp6.R
@@ -12,6 +13,7 @@ import com.scabher.guedrbootcamp6.model.Cities
 import kotlinx.android.synthetic.main.activity_city_pager.*
 
 class CityPagerActivity : AppCompatActivity() {
+    private val cities = Cities()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +21,6 @@ class CityPagerActivity : AppCompatActivity() {
 
         toolbar.setLogo(R.mipmap.ic_launcher)
         setSupportActionBar(toolbar)    // Fija como ActionBar el componente de la vista 'toolbar'
-
-        val cities = Cities()
 
         val adapter = object: FragmentPagerAdapter(supportFragmentManager) {
             override fun getItem(position: Int): Fragment {
@@ -37,6 +37,19 @@ class CityPagerActivity : AppCompatActivity() {
         }
 
         view_pager.adapter = adapter
+
+        view_pager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) { }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) { }
+
+            override fun onPageSelected(position: Int) {
+                updateCityInfo(position)
+            }
+
+        })
+
+        updateCityInfo(0)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -69,5 +82,9 @@ class CityPagerActivity : AppCompatActivity() {
         nextMenu?.isEnabled = view_pager.currentItem < adapter.count - 1
 
         return true
+    }
+
+    private fun updateCityInfo(position: Int) {
+        supportActionBar?.title = cities.getCity(position).name
     }
 }
